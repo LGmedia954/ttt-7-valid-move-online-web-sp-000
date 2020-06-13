1cffffe4c4bce42552100c297a461701ba944c3b
Leslie G <lesliegonzalez@me.com>
2020-06-13 12:32:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -24,7 +24,8 @@ def valid_move?(board, index)
     return false
   end
   
-  
-if index.between?(0, 8) && !position_taken?(board, index) == true
+def valid_move?(board, index)
+  num.between?(0, 8) && !position_taken?(board, index)
+end
 
 end
