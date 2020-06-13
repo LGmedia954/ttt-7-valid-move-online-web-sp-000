29bd835f816672cc121be646729f0c5a9657fe68
Leslie G <lesliegonzalez@me.com>
2020-06-13 06:52:27 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,24 @@
 # code your #valid_move? method here
 
+def valid_move?(board, index)
+  def position_taken?(array, slot)
+    if array[slot] == " " || array[slot] == "" || array[slot] == nil
+    return false
+    elsif
+      (array[slot] == "X") || (array[slot] == "O") == true
+    return true
+    end
+  end
+    
+ def on_board?(num)   
+    if gets.strip.to_i - 1 == num.between?(0, 8)
+    return true
+    else
+    return false
+    end
+ end
+ 
+ 
+ end
+ 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
