37b6743165ff2f8c6310a6737eac1980fb5e8107
Leslie G <lesliegonzalez@me.com>
2020-06-13 12:37:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -18,11 +18,7 @@ def valid_move?(board, index)
     end
  end
  
- if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
-  else
-    return false
-  end
+ 
   
 
 
